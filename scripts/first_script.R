2667bff9f8fb19ced2e8ed5332ef9bbd12601809
Daan Corzaan <p.d.corzaan@student.rug.nl>
2023-11-13 12:05:35 +0100

    project initialized with renv

--- scripts/first_script.R
+++ scripts/first_script.R
@@ -1,1 +1,12 @@
-# my first github synced script
+# my first github synced script 
+library(renv)
+
+#restore the versions of the packaged used to develop this project
+renv::restore()
+
+x<-c(1,2,3,4,5)
+y<-c(1,4,3,6,9)
+data<-data.frame(x,y)
+data
+
+
