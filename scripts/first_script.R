897f7ed0a0554c83c644fd25f1d18abad151b2bf
Daan Corzaan <p.d.corzaan@student.rug.nl>
2023-11-13 12:21:15 +0100

    made a figure

--- scripts/first_script.R
+++ scripts/first_script.R
@@ -3,10 +3,16 @@ library(renv)
 
 #restore the versions of the packaged used to develop this project
 renv::restore()
-
+#remember to use pull in Git tab to get changes from Github
+#load libraries
+library(tidyverse)
 x<-c(1,2,3,4,5)
 y<-c(1,4,3,6,9)
 data<-data.frame(x,y)
 data
 
-
+fig01<- data |> ggplot(aes(x=x, y=y))+
+  geom_point()+
+  geom_smooth(method='lm')
+fig01
+ggsave("./figures/fig01.png", plot=fig01, width=1920, height=1068, units="px")
